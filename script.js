5d5e263a2472a7ad81c5e3e04e1e6095aed8f7cd
CarlaLourenco <cristinasilvacarla31@gmail.com>
2023-02-13 23:27:40 -0300

    Formulario-idade-nome

---
+++ script.js
@@ -0,0 +1,69 @@
+// // // // // var n1=20
+// // // // // var n2=15
+
+// // // // // function resultado(n1,n2){
+// // // // //     if(n1>n2)
+// // // // //     console.log("maior",n1)
+// // // // // else{
+// // // // //     console.log("menor",n2)
+// // // // // }
+// // // // // }
+
+// // // // // resultado(n1,n2)
+
+
+// // // // function gerarNumeros(numero){
+// // // // for(let  i=0;i<numero;i++){
+// // // //     console.log("o numero é",i)
+// // // // }
+
+// // // // }
+// // // // gerarNumeros(3)
+
+ function mostrarIdade(){
+     let idade = parseInt(document.getElementById("idade").value)
+     let nome = document.getElementById("nome").value
+     let resultado= document.getElementById("resultado")
+ resultado.innerHTML +=" seu nome é " + nome + " sua idade é : " + idade
+
+ } 
+
+// // // function operacoes(n1,n2){
+// // //    let somar =n1+n2; 
+// // //    let dividir =n1/n2; 
+// // //    let subtrair =n1-n2; 
+// // //    let multiplicar =n1*n2; 
+
+// // //    console.log(somar)
+// // //    console.log(dividir)
+// // //    console.log(subtrair)
+// // //    console.log(multiplicar)
+   
+// // // }
+
+// // // operacoes(200,20)
+
+// // function idade(idade){
+// //     if(idade>18)
+// //     console.log("maior de idade")
+// //     else{
+// // console.log("menor idade")
+
+// //     }
+// // }
+// // idade(2)
+
+
+// function verificarNotas(notas){
+//     if(notas>7)
+//     console.log("parabéns você foi aprovado")
+// else if(notas>5)
+// console.log("em recuperação")
+// else{
+//     console.log("reprovado")
+// }
+// }
+// verificarNotas(2)
+
+
+
